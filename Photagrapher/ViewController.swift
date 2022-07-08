9673a960efcc6ee3cf6defb70e463ac6931c4fbd
Richie Flores <rfloresc@icloud.com>
2022-07-08 11:02:22 -0700

    Initial Commit

---
+++ Photagrapher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Photagrapher
+//
+//  Created by Richie Flores on 7/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
