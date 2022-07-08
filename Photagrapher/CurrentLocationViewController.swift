6fa54f98af5dcde95e316272ce5ffe7f10773446
Richie Flores <rfloresc@icloud.com>
2022-07-08 11:18:08 -0700

    Embed in Tab Bar view controller. Add IBOutlets to the ViewController.

---
+++ Photagrapher/CurrentLocationViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Photagrapher
+//
+//  Created by Richie Flores on 7/8/22.
+//
+
+import UIKit
+
+class CurrentLocationViewController: UIViewController {
+  @IBOutlet weak var messageLabel: UILabel!
+  @IBOutlet weak var latitudeLabel: UILabel!
+  @IBOutlet weak var longitudeLabel: UILabel!
+  @IBOutlet weak var addressLabel: UILabel!
+  @IBOutlet weak var tagButton: UIButton!
+  @IBOutlet weak var getButton: UIButton!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+  
+  // MARK: - Actions
+  @IBAction func getLocation() {
+    // TODO: Add location location
+  }
+}
