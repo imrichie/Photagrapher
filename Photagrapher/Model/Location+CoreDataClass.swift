8ff38f5017bdb5ab20981d19db707e55aeca6b04
Richie Flores <rfloresc@icloud.com>
2022-08-01 22:11:50 -0700

    Add Core Data entity - Location and configured Core Data Stack in Scene Delegate

---
+++ Photagrapher/Model/Location+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Location+CoreDataClass.swift
+//  Photagrapher
+//
+//  Created by Richie Flores on 8/1/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Location)
+public class Location: NSManagedObject {
+
+}
