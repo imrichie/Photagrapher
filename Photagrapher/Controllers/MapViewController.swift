365ee2bc4f7bd8c07c3fbbc3467da9874f530466
Richie Flores <rfloresc@icloud.com>
2022-08-13 00:45:33 -0700

    Add a new MapViewController to project

---
+++ Photagrapher/Controllers/MapViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MapViewController.swift
+//  Photagrapher
+//
+//  Created by Richie Flores on 8/13/22.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
