1d6ba0a6ff689b568f745cafe0bea2c69f8e368a
Richie Flores <rfloresc@icloud.com>
2022-08-18 21:46:05 -0700

    Calculate the center of all Locations and center region around it

--- Photagrapher/Controllers/MapViewController.swift
+++ Photagrapher/Controllers/MapViewController.swift
@@ -18,6 +18,9 @@ class MapViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     updateLocations()
+    if !locationManager.locations.isEmpty {
+      showLocations()
+    }
   }
   
   // MARK: - Bar Button Actions
@@ -26,21 +29,53 @@ class MapViewController: UIViewController {
     mapView.setRegion(region, animated: true)
   }
   
-  @IBAction func showLocations(_ sender: Any) {
+  @IBAction func showLocations() {
     print(">>> Showing Locations...")
+    let region = region(for: locationManager.locations)
+    mapView.setRegion(region, animated: true)
   }
   
   // MARK: - Helper methods
   func updateLocations() {
     mapView.removeAnnotations(locationManager.locations)
     let fetchRequest = NSFetchRequest<Location>(entityName: Constants.Entities.location)
-    
-    do {
-      locationManager.locations = try locationManager.managedObjectContext.fetch(fetchRequest)
-    } catch {
-      fatalError(">>> FETCHING ERROR: \(error.localizedDescription)")
-    }
+    locationManager.locations = try! locationManager.managedObjectContext.fetch(fetchRequest)
     mapView.addAnnotations(locationManager.locations)
+  }
+  
+  func region(for annotations: [MKAnnotation]) -> MKCoordinateRegion {
+    let region: MKCoordinateRegion
+
+    switch annotations.count {
+    case 0:
+      region = MKCoordinateRegion(center: mapView.userLocation.coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
+
+    case 1:
+      let annotation = annotations[annotations.count - 1]
+      region = MKCoordinateRegion(center: annotation.coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
+
+    default:
+      var topLeft = CLLocationCoordinate2D(latitude: -90, longitude: 180)
+      var bottomRight = CLLocationCoordinate2D(latitude: 90, longitude: -180)
+
+      for annotation in annotations {
+        topLeft.latitude = max(topLeft.latitude, annotation.coordinate.latitude)
+        topLeft.longitude = min(topLeft.longitude, annotation.coordinate.longitude)
+        bottomRight.latitude = min(bottomRight.latitude, annotation.coordinate.latitude)
+        bottomRight.longitude = max(bottomRight.longitude, annotation.coordinate.longitude)
+      }
+
+      let center = CLLocationCoordinate2D(
+        latitude: topLeft.latitude - (topLeft.latitude - bottomRight.latitude) / 2,
+        longitude: topLeft.longitude - (topLeft.longitude - bottomRight.longitude) / 2)
+
+      let extraSpace = 1.1
+      let span = MKCoordinateSpan(latitudeDelta: abs(topLeft.latitude - bottomRight.latitude) * extraSpace, longitudeDelta: abs(topLeft.longitude - bottomRight.longitude) * extraSpace)
+
+      region = MKCoordinateRegion(center: center, span: span)
+    }
+
+    return mapView.regionThatFits(region)
   }
 }
 
